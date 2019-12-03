187251d989b40f7425f740154f7564b269fabdb0
yingyangle <cyang1999@gmail.com>
2019-12-03 17:14:34 -0500

    save as pickle

--- features/getFeatures.py
+++ features/getFeatures.py
@@ -6,14 +6,14 @@
 # profanity word list based on:
 # https://github.com/areebbeigh/profanityfilter/
 
-import os, re, nltk, pandas as pd, numpy as np
+import os, re, nltk, pandas as pd, numpy as np, pickle
 from nltk.corpus import words
 from nltk.corpus import stopwords
 from getData import splitData, getLines, getCast, normalizeData
 from topWords import getTopWords
 
-# your_path = '/Users/Christine/cs/whosaidthat' # christine
-your_path = '/Users/user/NLP Project/whosaidthat-1'  # dora
+your_path = '/Users/Christine/cs/whosaidthat' # christine
+# your_path = '/Users/user/NLP Project/whosaidthat-1'  # dora
 
 allwords = words.words()  # all english words
 stopwords = stopwords.words('english')  # stop words
@@ -22,8 +22,8 @@ ein = open(your_path + '/features/profanity.txt', 'r')
 profanity = ein.read().rstrip().split('\n')  # profanity words
 ein.close()
 
-############################# helper functions ################################
 
+############################# helper functions ################################
 
 # utterance type
 def utterType(line):
@@ -32,18 +32,15 @@ def utterType(line):
     ellipses = line.count('...')
     return [questions, exclamations, ellipses]
 
-
 # ratio of neologisms to words in utterance
 def neologisms(line):
     neos = [x for x in line if x not in allwords]
     return len(neos) / len(line)
 
-
 # ratio of stop words to words in utterance
 def stopwordsratio(line):
     stops = [x for x in line if x in stopwords]
     return len(stops) / len(line)
-
 
 # how many top words this line contains
 def numTopWords(line, top_words):
@@ -52,14 +49,12 @@ def numTopWords(line, top_words):
         if s in top_words: ans += 1
     return ans
 
-
 # contains numbers
 # 1 = contains numbers, 0 = doesn't contain numbers
 def hasNumbers(line):
     for s in line:
         if s.isdigit(): return 1
     return 0
-
 
 # contains profanity
 # 1 = contains profanity, 0 = doesn't contain profanity
@@ -68,17 +63,16 @@ def hasProfanity(line):
         if p in line: return 1
     return 0
 
-
 ############################# helper functions ################################
 
-############################# main ################################
 
+############################# main ################################
 
 # get features for list of lines of dialogue
 # takes in list of lists of tokens, returns list of feature lists
 def getFeatures(lines, top_words):
-    features = []  # full list of features for each line
-    for line in lines:  # for each line
+    features = [] # full list of features for each line
+    for line in lines: # for each line
         line_features = np.array([ # features for this line
             len(line), # utterance len
             len(''.join(line)) / len(line), # avg word len
@@ -87,69 +81,59 @@ def getFeatures(lines, top_words):
             neologisms(line), # ratio of neologisms to words
             stopwordsratio(line), # stopwords ratio
             hasNumbers(line), # has number=1, ow=0
-            hasProfanity(line) # has profanity=1, ow=0
-        ])
-        line_features = np.concatenate((line_features,
-                                        utterType(line)))  # num of ? ! ...)
-        features.append(line_features)  # add this line's features to full list
+            hasProfanity(line)]) # has profanity=1, ow=0
+        line_features = np.concatenate((line_features,utterType(line)))  # num of ? ! ...)
+        features.append(line_features) # add this line's features to full list
     return features
-
 
 # convert speaker/line df to features/label df for given character
 # label is 1 if line spoken by the focus character, 0 otherwise
 def convertToFeatures(df, character, top_words):
-    # ones_lines = getLines(df, character)[:10]  # lines for focus character, only 10 lines
-    ones_lines = getLines(df, character) # lines for focus character, full
+    ones_lines = getLines(df, character)[:10]  # lines for focus character, only 10 lines
+    # ones_lines = getLines(df, character) # lines for focus character, full
     ones_lines = normalizeData(ones_lines)
-    ones_feats = getFeatures(ones_lines,
-                             top_words)  # features for focus character's lines
-    ones = np.ones_like(ones_lines)  # list of 1's labels
+    ones_feats = getFeatures(ones_lines, top_words) # features for focus character's lines
+    ones = np.ones_like(ones_lines) # list of 1's labels
 
-    cast = getCast(df)  # list of main characters in show
-    cast.remove(character)  # remove focus character from character list
-    # zeros_lines = getLines(df, cast)[:10]  # lines for remaining characters, only 10 lines
-    zeros_lines = getLines(df, cast)  # lines for remaining characters, full
+    cast = getCast(df) # list of main characters in show
+    cast.remove(character) # remove focus character from character list
+    zeros_lines = getLines(df, cast)[:10] # lines for remaining characters, only 10 lines
+    # zeros_lines = getLines(df, cast) # lines for remaining characters, full
     zeros_lines = normalizeData(zeros_lines)
-    zeros_feats = getFeatures(
-        zeros_lines, top_words)  # features for remainin characters' lines
-    zeros = np.zeros_like(zeros_lines)  # list of 0's labels
+    zeros_feats = getFeatures(zeros_lines, top_words) # features for remainin characters' lines
+    zeros = np.zeros_like(zeros_lines) # list of 0's labels
 
     labels = list(np.concatenate((ones, zeros), axis=0).flatten())
     feats = list(np.concatenate((ones_feats, zeros_feats), axis=0))
     # print(character, 'Datapoints:', len(ones), ', Total Datapoints:', len(labels))
     data = {'Label': labels, 'Features': feats}
-    df = pd.DataFrame(
-        data)  # dataframe of features for each line and 1/0 label
+    df = pd.DataFrame(data) # dataframe of features for each line and 1/0 label
     return df
 
 
 # create train and test dataset for given show and character
-# write resulting datsets to .csv and return dfs
+# write resulting datsets to .pkl and return dfs
 def createDataset(filename, character):
-    train, test = splitData(filename,
-                            0.2)  # split 80/20 for training/testing data
+    train, test = splitData(filename, 0.2)  # split 80/20 for training/testing data
     char_lines = getLines(train, character)  # get character's lines
-    top_words = getTopWords(char_lines,
-                            20)  # 20 most frequent words for character
+    top_words = getTopWords(char_lines, 20)  # 20 most frequent words for character
     train = convertToFeatures(train, character, top_words)
     test = convertToFeatures(test, character, top_words)
     # save to .csv
-    output = filename[:-4] + character
-    train.to_csv(
-        'datasets/' + output + 'Train.csv', index=False, encoding='utf-8-sig')
-    test.to_csv(
-        'datasets/' + output + 'Test.csv', index=False, encoding='utf-8-sig')
+    name = filename[:-4] + character
+    train.to_pickle('datasets/' + name + 'Train.pkl')
+    test.to_pickle('datasets/' + name + 'Test.pkl')
     return (train, test)
 
-
 ############################# main ################################
+
 
 ############################# execute ################################
 
 os.chdir(your_path)
 
 # create dataset for Big Bang and Sheldon - done！
-# train, test = createDataset('bang.csv', 'Sheldon')
+train, test = createDataset('bang.csv', 'Sheldon')
 # create dataset for Big Bang and Penny - done!
 # train, test = createDataset('bang.csv', 'Penny')
 # # create dataset for Big Bang and Leonard - done！
@@ -177,7 +161,3 @@ os.chdir(your_path)
 
 # create dataset for desperate and Susan - done
 # train, test = createDataset('desperate.csv', 'Susan')
-
-
-
-
--- features/logreg.py
+++ features/logreg.py
@@ -7,26 +7,26 @@ import os, re, nltk, pandas as pd, numpy as np
 from getFeatures import createDataset
 from sklearn.linear_model import LogisticRegression
 
-# your_path = '/Users/Christine/cs/whosaidthat' # christine
-your_path = '/Users/user/NLP Project/whosaidthat-1'  # dora
-os.chdir(your_path + '/features')
+your_path = '/Users/Christine/cs/whosaidthat' # christine
+# your_path = '/Users/user/NLP Project/whosaidthat' # dora
+os.chdir(your_path+'/features')
 os.chdir(your_path)
 
-train, test = createDataset('bang.csv', 'Sheldon')
-
-
+train = pd.read_pickle('datasets/bangSheldonTrain.pkl')
+test = pd.read_pickle('datasets/bangSheldonTest.pkl')
 
 # training data
-X_train = np.array([x for x in train.values[:, 1]])
-y_train = train.values[:, 0]
+X_train = np.array([x for x in train.values[:,1]])
+y_train = train.values[:,0]
 y_train = y_train.astype('int')
 
 # testing data
-X_test = np.array([x for x in test.values[:, 1]])
-y_test = train.values[:, 0]
+X_test = np.array([x for x in test.values[:,1]])
+y_test = train.values[:,0]
 y_test = y_test.astype('int')
 
 log = LogisticRegression(solver='liblinear', multi_class='ovr')
 log.fit(X_train, y_train)
 predictions = log.predict(X_test)
-log.score(X_test, y_test)
+score = log.score(X_test, y_test)
+print('Accuracy:', score)
